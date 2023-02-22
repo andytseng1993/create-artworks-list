3a5475392f09ec74258156cab76fdad492b3585a
Andy Tseng <anby1611@gmail.com>
2023-02-22 00:10:12 -0800

    Build material api logic

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import material from './routes/api/material'
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 3002
 
 app.use(express.json())
-
+app.use('/api/material', material)
 app.listen(port, () => console.log(`Sever started on port ${port}`))
