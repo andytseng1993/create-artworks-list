23027c8c49cabee8bc15dc0955fbfa1c87c504e2
Andy Tseng <anby1611@gmail.com>
2023-02-22 01:13:43 -0800

    Change front end proxy to loacalhost

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import material from './routes/api/material'
+import materialOption from './routes/api/material.js'
+import typeOption from './routes/api/type.js'
+import { PrismaClient } from '@prisma/client'
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 3002
 
 app.use(express.json())
-app.use('/api/material', material)
+app.use('/api/material', materialOption)
+app.use('/api/type', typeOption)
+
 app.listen(port, () => console.log(`Sever started on port ${port}`))
