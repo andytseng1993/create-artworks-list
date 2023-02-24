69c78cff104365f9f1634c64bdedf6677b731272
Andy Tseng <anby1611@gmail.com>
2023-02-23 23:35:29 -0800

    update vercel

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import materialOption from './routes/api/material.js'
 import typeOption from './routes/api/type.js'
-import { PrismaClient } from '@prisma/client'
+import path from 'path'
 
 dotenv.config()
 const app = express()
@@ -12,4 +12,8 @@ app.use(express.json())
 app.use('/api/material', materialOption)
 app.use('/api/type', typeOption)
 
+app.use(express.static(path.join(__dirname, './client/dist')))
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, './client/dist/index.html'))
+})
 app.listen(port, () => console.log(`Sever started on port ${port}`))
